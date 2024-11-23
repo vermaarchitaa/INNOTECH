e9ba1da7b97c3f7b45d668277684694db455e3fc
vermaarchitaa <architav635@gmail.com>
2024-11-23 09:06:21 +0530

    update

--- practice2/script.js
+++ practice2/script.js
@@ -1,42 +1,3 @@
-/*
-const states = document.querySelectorAll('.state');
-
-const overlay = document.getElementById('overlay');
-const fullImage = document.getElementById('fullImage');
-const closeBtn = document.getElementById('closeBtn');
-
-states.forEach(state => {
-  state.addEventListener('mouseenter', () => {
-    const imgSrc = state.querySelector('img').src;
-    document.body.style.backgroundImage = url(${imgSrc});
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundPosition = 'center';
-    document.body.style.filter = 'none';
-  });
-
-  state.addEventListener('mouseleave', () => {
-    document.body.style.backgroundImage = '';
-  });
-
-  state.addEventListener('click', () => {
-    const imgSrc = state.querySelector('img').src;
-    fullImage.src = imgSrc;
-    overlay.style.display = 'flex';  
-  });
-});
-
-closeBtn.addEventListener('click', () => {
-  overlay.style.display = 'none';
-});
-
-overlay.addEventListener('click', (e) => {
-  if (e.target === overlay) {
-    overlay.style.display = 'none';
-  }
-}); */
-
-
-
 const states = document.querySelectorAll('.state');
 
 const overlay = document.getElementById('overlay');
@@ -72,7 +33,3 @@ overlay.addEventListener('click', (e) => {
     overlay.style.display = 'none';
   }
 });
-
-//<div class="Signup">
-//<p>Sign Up</p>
-//</div> 
